ea6acc9db7c07ae4190cd3f2b2a1deaf9575c300
Alaa-essam13 <alaaessamebrahim@gmail.com>
2025-05-30 03:41:03 +0300

    adding category controller

---
+++ src/main/java/org/example/springsec/ecomm/controller/CategoryController.java
@@ -0,0 +1,33 @@
+package org.example.springsec.ecomm.controller;
+
+import lombok.AllArgsConstructor;
+import org.example.springsec.ecomm.dto.CategoryDto;
+import org.example.springsec.ecomm.service.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/api/v1/category")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<CategoryDto>> getCategoryDto() {
+        return ResponseEntity.ok(categoryService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDto> getCategoryDtoById(@PathVariable Long id){
+        return ResponseEntity.ok(categoryService.getById(id));
+    }
+
+    @PutMapping("/add")
+    public ResponseEntity<Void> putCategoryDto(@RequestBody CategoryDto categoryDto) {
+        return categoryService.addCategory(categoryDto);
+    }
+
+}
