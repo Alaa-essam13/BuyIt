c958b7cac4607b3cf83040ffb4a9a2e3d21736c5
Alaa-essam13 <alaaessamebrahim@gmail.com>
2025-06-20 23:57:31 +0300

    m

--- src/main/java/org/example/springsec/ecomm/repo/ReviewRepo.java
+++ src/main/java/org/example/springsec/ecomm/repo/ReviewRepo.java
@@ -2,7 +2,15 @@ package org.example.springsec.ecomm.repo;
 
 import org.example.springsec.ecomm.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 public interface ReviewRepo extends JpaRepository<Review, Long> {
+
+    @Query("""
+        select r from Review r where r.product.id = :id
+""")
+    List<Review> findByProductId(Long id);
 }
