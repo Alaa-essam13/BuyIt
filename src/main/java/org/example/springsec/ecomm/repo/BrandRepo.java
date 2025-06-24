51422d12247ef47a5852f3e4d9b4b97d9ef02b71
Alaa-essam13 <alaaessamebrahim@gmail.com>
2025-06-24 23:29:06 +0300

    testtkn

--- src/main/java/org/example/springsec/ecomm/repo/BrandRepo.java
+++ src/main/java/org/example/springsec/ecomm/repo/BrandRepo.java
@@ -1,8 +1,23 @@
 package org.example.springsec.ecomm.repo;
 
 import org.example.springsec.ecomm.entity.Brand;
+import org.example.springsec.ecomm.entity.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 public interface BrandRepo extends JpaRepository<Brand, Long> {
+
+
+    @EntityGraph(attributePaths = {
+            "products"
+    })
+    @Query("""
+        select b.products from Brand b where b.id = :b_id
+""")
+    List<Product> getProductsByBrandId(Long b_id);
+
 }
